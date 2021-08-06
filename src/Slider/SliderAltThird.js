bdae30d3922994bb716865c580cd0ede3637b068
dna-user <dna-user@pop-os.localdomain>
2021-08-06 13:50:33 +0800

    installing axios and creating another slider

---
+++ src/Slider/SliderAltThird.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SliderAltThird = () => {
+    const url = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json'
+
+    
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SliderAltThird
