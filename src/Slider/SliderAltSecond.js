d4c9d10940c24e588b44fe4ac3830d6988347a47
dna-user <dna-user@pop-os.localdomain>
2021-08-06 10:04:11 +0800

    making minor changes on the SliderAltSecond.js

--- src/Slider/SliderAltSecond.js
+++ src/Slider/SliderAltSecond.js
@@ -31,7 +31,7 @@ const SliderAltSecond = () => {
                 key={index}
             >
                 {index === current && (
-                <img src={slide.image} alt='travel image' className='image' />
+                    <img src={slide.image} alt='travel image' className='image' />
                 )}
             </div>
             );
